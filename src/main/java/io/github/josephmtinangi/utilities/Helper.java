c8e7938d20659e0a8018e63d6bd8012803588308
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-03-20 11:32:57 +0300

    Create response helper

---
+++ src/main/java/io/github/josephmtinangi/utilities/Helper.java
@@ -0,0 +1,13 @@
+package io.github.josephmtinangi.utilities;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class Helper {
+
+	public static ResponseEntity<?> createResponse(Object data, HttpStatus code) {
+
+		return new ResponseEntity<>(data, code);
+	}
+
+}
