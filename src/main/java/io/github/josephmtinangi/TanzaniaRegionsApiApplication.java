b5a11112e971fabb1698415f8447e72cacb2f2dd
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-03-20 11:07:55 +0300

    Initial commit

---
+++ src/main/java/io/github/josephmtinangi/TanzaniaRegionsApiApplication.java
@@ -0,0 +1,12 @@
+package io.github.josephmtinangi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanzaniaRegionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanzaniaRegionsApiApplication.class, args);
+	}
+}
