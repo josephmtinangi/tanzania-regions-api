694c669b1fc3a381c2940cae1bda712669c7d5d9
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-03-20 12:37:29 +0300

    Create Ward model and repository

---
+++ src/main/java/io/github/josephmtinangi/repositories/WardRepository.java
@@ -0,0 +1,11 @@
+package io.github.josephmtinangi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.josephmtinangi.models.Ward;
+
+@Repository
+public interface WardRepository extends JpaRepository<Ward, Long> {
+
+}
