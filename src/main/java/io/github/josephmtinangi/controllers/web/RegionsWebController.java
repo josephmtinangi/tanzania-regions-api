510ec54e655d3a9fcd80c21e73b7179e792ea179
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-03-31 16:43:42 +0300

    Upload regions

--- src/main/java/io/github/josephmtinangi/controllers/web/RegionsWebController.java
+++ src/main/java/io/github/josephmtinangi/controllers/web/RegionsWebController.java
@@ -1,21 +1,41 @@
 package io.github.josephmtinangi.controllers.web;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.opencsv.CSVReader;
 
 import io.github.josephmtinangi.models.Region;
 import io.github.josephmtinangi.repositories.RegionRepository;
+import io.github.josephmtinangi.utilities.Helper;
 
 @Controller
 @RequestMapping(path = "/dashboard/regions")
 public class RegionsWebController {
+
+	private static final Logger log = LoggerFactory.getLogger(RegionsWebController.class);
+
+	@Value("${app.rootStorage}")
+	private String rootStorage;
 
 	@Autowired
 	private RegionRepository regionRepository;
@@ -50,6 +70,49 @@ public class RegionsWebController {
 	public String store(@ModelAttribute Region region) {
 
 		regionRepository.save(region);
+
+		return "redirect:/dashboard/regions";
+	}
+
+	@RequestMapping(path = "/upload", method = RequestMethod.POST)
+	public String upload(@RequestParam MultipartFile file) throws Exception {
+
+		if (file != null && !file.isEmpty()) {
+
+			try {
+
+				String filepath = rootStorage + "/" + Helper.saveFile(file, rootStorage);
+				log.info("filepath = " + filepath);
+
+				File f = new File(filepath);
+
+				Path path = Paths.get(f.getAbsolutePath());
+				log.info("path = " + path);
+
+				Reader reader = Files.newBufferedReader(path);
+				CSVReader csvReader = new CSVReader(reader);
+
+				String[] nextLine;
+
+				List<Region> regionsList = new ArrayList<>();
+				while ((nextLine = csvReader.readNext()) != null) {
+					log.info(nextLine[0]);
+					Region region = new Region();
+					region.setName(nextLine[0]);
+					region.setSlug(nextLine[0]);
+
+					regionsList.add(region);
+				}
+
+				regionRepository.save(regionsList);
+
+				csvReader.close();
+
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
 
 		return "redirect:/dashboard/regions";
 	}
--- src/main/resources/templates/regions/create.ftl
+++ src/main/resources/templates/regions/create.ftl
@@ -1,6 +1,31 @@
 <@includes>
 
 <h1 class="page-header">Add Region</h1>
+
+ <a class="btn btn-primary" data-toggle="modal" href='#modal-id'>Upload Regions</a>
+  <br /><br />
+ <div class="modal fade" id="modal-id">
+ 	<div class="modal-dialog">
+ 		<form action="<@spring.url '/dashboard/regions/upload'/>" method="POST" enctype="multipart/form-data" role="form">
+ 		<div class="modal-content">
+ 			<div class="modal-header">
+ 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+ 				<h4 class="modal-title">Upload Regions</h4>
+ 			</div>
+ 			<div class="modal-body">
+	 			<div class="form-group">
+	 				<label for="file">File</label>
+	 				<input type="file" name="file" id="file" required>
+	 			</div> 				
+ 			</div>
+ 			<div class="modal-footer">
+ 				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+ 				<button type="submit" class="btn btn-primary">Upload</button>
+ 			</div>
+ 		</div>
+ 		</form> 		
+ 	</div>
+ </div>
  
 <form action="<@spring.url '/dashboard/regions'/>" method="POST" enctype="multipart/form-data" role="form">
 	
