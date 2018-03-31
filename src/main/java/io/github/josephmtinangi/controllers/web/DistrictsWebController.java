5f19b8872c13fbd3090233d145944181a1e4dab1
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-03-31 17:02:57 +0300

    Add districts

--- src/main/java/io/github/josephmtinangi/controllers/web/DistrictsWebController.java
+++ src/main/java/io/github/josephmtinangi/controllers/web/DistrictsWebController.java
@@ -1,11 +1,15 @@
 package io.github.josephmtinangi.controllers.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.github.josephmtinangi.models.District;
 import io.github.josephmtinangi.models.Region;
@@ -16,22 +20,46 @@ import io.github.josephmtinangi.repositories.RegionRepository;
 @RequestMapping(path = "/dashboard/regions")
 public class DistrictsWebController {
 
-    @Autowired
-    private RegionRepository regionRepository;
+	@Autowired
+	private RegionRepository regionRepository;
 
-    @Autowired
-    private DistrictRepository districtRepository;
+	@Autowired
+	private DistrictRepository districtRepository;
 
-    @RequestMapping(path = "/{regionId}/districts/{districtId}", method = RequestMethod.GET)
-    public String show(Model model, @PathVariable Long regionId, @PathVariable Long districtId) {
+	@RequestMapping(path = "/{regionId}/districts/{districtId}", method = RequestMethod.GET)
+	public String show(Model model, @PathVariable Long regionId, @PathVariable Long districtId) {
 
-        Region region = regionRepository.findOne(regionId);
+		Region region = regionRepository.findOne(regionId);
 
-        District district = districtRepository.findFirstByIdAndRegionId(districtId, region.getId());
+		District district = districtRepository.findFirstByIdAndRegionId(districtId, region.getId());
 
-        model.addAttribute("district", district);
+		model.addAttribute("district", district);
 
-        return "districts/show";
-    }
+		return "districts/show";
+	}
+
+	@RequestMapping(path = "/{regionId}/districts", method = RequestMethod.POST)
+	public String store(@PathVariable Long regionId, @RequestParam String districtsString) {
+
+		Region region = regionRepository.findOne(regionId);
+
+		String[] districts = districtsString.split(",");
+
+		List<District> districtList = new ArrayList<>();
+		if (districts != null && districts.length > 0) {
+			for (String name : districts) {
+				District district = new District();
+				district.setName(name);
+				district.setSlug(name);
+				district.setRegion(region);
+
+				districtList.add(district);
+			}
+		}
+
+		districtRepository.save(districtList);
+
+		return "redirect:/dashboard/regions/" + region.getId();
+	}
 
 }
--- src/main/resources/templates/regions/show.ftl
+++ src/main/resources/templates/regions/show.ftl
@@ -1,6 +1,31 @@
 <@includes>
 
  <h1 class="page-header">${region.name}</h1>
+ 
+  <a class="btn btn-primary" data-toggle="modal" href='#modal-id'>Add Districts</a>
+  <br /><br />
+ <div class="modal fade" id="modal-id">
+ 	<div class="modal-dialog">
+ 		<form action="<@spring.url '/dashboard/regions/${region.id}/districts'/>" method="POST" role="form">
+ 		<div class="modal-content">
+ 			<div class="modal-header">
+ 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+ 				<h4 class="modal-title">Add Districts</h4>
+ 			</div>
+ 			<div class="modal-body">
+	 			<div class="form-group">
+	 				<label for="districtsString">Districts</label>
+	 				<textarea name="districtsString" class="form-control" rows="10"></textarea>
+	 			</div> 				
+ 			</div>
+ 			<div class="modal-footer">
+ 				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+ 				<button type="submit" class="btn btn-primary">Upload</button>
+ 			</div>
+ 		</div>
+ 		</form> 		
+ 	</div>
+ </div>
 
  <table class="table table-bordered">
      <thead>
