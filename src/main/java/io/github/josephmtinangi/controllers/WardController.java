af1a08dc0d57316c2121b02b3e5f298909176dce
Joseph Mtinangi <josephmtinangi@gmail.com>
2018-05-14 12:01:54 +0300

    Create Ward

--- src/main/java/io/github/josephmtinangi/controllers/WardController.java
+++ src/main/java/io/github/josephmtinangi/controllers/WardController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.github.josephmtinangi.models.District;
 import io.github.josephmtinangi.models.Region;
@@ -22,52 +20,71 @@ import io.github.josephmtinangi.utilities.Helper;
 @RequestMapping(path = "/regions/{regionSlug}/districts/{districtSlug}/wards")
 public class WardController {
 
-	@Value("${app.url}")
-	private String appURL;
+    @Value("${app.url}")
+    private String appURL;
 
-	@Autowired
-	private RegionRepository regionRepository;
+    @Autowired
+    private RegionRepository regionRepository;
 
-	@Autowired
-	private DistrictRepository districtRepository;
+    @Autowired
+    private DistrictRepository districtRepository;
 
-	@Autowired
-	private WardRepository wardRepository;
+    @Autowired
+    private WardRepository wardRepository;
 
-	@RequestMapping(path = "/{wardSlug}")
-	public ResponseEntity<?> show(@PathVariable String regionSlug, @PathVariable String districtSlug,
-			@PathVariable String wardSlug) {
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public ResponseEntity<?> store(@PathVariable String regionSlug, @PathVariable String districtSlug, @ModelAttribute Ward ward) {
 
-		Region region = regionRepository.findFirstBySlug(regionSlug);
+        Region region = regionRepository.findFirstBySlug(regionSlug);
+        if (region == null) {
+            return Helper.createResponse(Helper.createMessage("Region not found"), HttpStatus.BAD_REQUEST);
+        }
 
-		if (region == null) {
-			HashMap<String, Object> outputMap = new HashMap<>();
-			outputMap.put("message", "Not Found");
-			return Helper.createResponse(outputMap, HttpStatus.BAD_REQUEST);
-		}
+        District district = districtRepository.findByRegionIdAndSlug(region.getId(), districtSlug);
+        if (district == null) {
+            return Helper.createResponse(Helper.createMessage("District not found"), HttpStatus.BAD_REQUEST);
+        }
 
-		District district = districtRepository.findByRegionIdAndSlug(region.getId(), districtSlug);
+        ward.setDistrict(district);
+        Ward newWard = wardRepository.save(ward);
 
-		if (district == null) {
-			HashMap<String, Object> outputMap = new HashMap<>();
-			outputMap.put("message", "Not Found");
-			return Helper.createResponse(outputMap, HttpStatus.BAD_REQUEST);
-		}
+        return Helper.createResponse(newWard, HttpStatus.CREATED);
+    }
 
-		Ward ward = wardRepository.findByDistrictIdAndSlug(district.getId(), wardSlug);
+    @RequestMapping(path = "/{wardSlug}")
+    public ResponseEntity<?> show(@PathVariable String regionSlug, @PathVariable String districtSlug,
+                                  @PathVariable String wardSlug) {
 
-		if (ward == null) {
-			HashMap<String, Object> outputMap = new HashMap<>();
-			outputMap.put("message", "Not Found");
-			return Helper.createResponse(outputMap, HttpStatus.BAD_REQUEST);
-		}
+        Region region = regionRepository.findFirstBySlug(regionSlug);
 
-		HashMap<String, Object> wardMap = new HashMap<>();
-		wardMap.put("id", ward.getId());
-		wardMap.put("name", ward.getName());
-		wardMap.put("url", appURL + "/regions/" + district.getRegion().getSlug() + "/districts/" + district.getSlug()
-				+ "/wards/" + ward.getSlug());
+        if (region == null) {
+            HashMap<String, Object> outputMap = new HashMap<>();
+            outputMap.put("message", "Not Found");
+            return Helper.createResponse(outputMap, HttpStatus.BAD_REQUEST);
+        }
 
-		return Helper.createResponse(wardMap, HttpStatus.OK);
-	}
+        District district = districtRepository.findByRegionIdAndSlug(region.getId(), districtSlug);
+
+        if (district == null) {
+            HashMap<String, Object> outputMap = new HashMap<>();
+            outputMap.put("message", "Not Found");
+            return Helper.createResponse(outputMap, HttpStatus.BAD_REQUEST);
+        }
+
+        Ward ward = wardRepository.findByDistrictIdAndSlug(district.getId(), wardSlug);
+
+        if (ward == null) {
+            HashMap<String, Object> outputMap = new HashMap<>();
+            outputMap.put("message", "Not Found");
+            return Helper.createResponse(outputMap, HttpStatus.BAD_REQUEST);
+        }
+
+        HashMap<String, Object> wardMap = new HashMap<>();
+        wardMap.put("id", ward.getId());
+        wardMap.put("name", ward.getName());
+        wardMap.put("url", appURL + "/regions/" + district.getRegion().getSlug() + "/districts/" + district.getSlug()
+                + "/wards/" + ward.getSlug());
+
+        return Helper.createResponse(wardMap, HttpStatus.OK);
+    }
 }
